78a57c9d6479db2f8e567bdeff623eac93301710
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-05-30 20:41:56 -0500

    gesture detector

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -40,54 +40,65 @@ class HomePage extends StatelessWidget {
   ];
 
   Widget buildMenuCard(double widthPageAux, Map<String, dynamic> menuMap) {
-    return Container(
-      margin: EdgeInsets.only(top: 16),
-      padding: EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(color: Colors.black, width: 2),
-      ),
-      child: Row(
-        children: [
-          // PRIMERA OPCIÓN PARA PONER BORDER REDONDEADOS EN UNA IMAGEN
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.network(
-              menuMap["urlImage"],
-              width: widthPageAux / 4,
-              height: widthPageAux / 4,
-              fit: BoxFit.cover,
-            ),
-          ),
-
-          // SEGUNDA FORMA PARA PONER BORDES REONDEADOS PARA UNA IMAGEN
-          // Container(
-          //   width: widthPage / 4,
-          //   height: widthPage / 4,
-          //   decoration: BoxDecoration(
-          //     borderRadius: BorderRadius.circular(12),
-          //     image: DecorationImage(
-          //       image: NetworkImage(
-          //         "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          //       ),
-          //       fit: BoxFit.cover,
-          //     ),
-          //   ),
-          // ),
-          SizedBox(width: 16),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(menuMap["nombre"], style: TextStyle(fontSize: 22)),
-              Text(menuMap["dias"], style: TextStyle(fontSize: 18)),
-              Text(
-                "S/ . ${menuMap["precio"]} ",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: () {
+        print(menuMap);
+      },
+      onDoubleTap: () {
+        print("Dos taps");
+      },
+      onLongPress: () {
+        print("Long presss ${menuMap["nombre"]}");
+      },
+      child: Container(
+        margin: EdgeInsets.only(top: 10),
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(color: Colors.black, width: 2),
+        ),
+        child: Row(
+          children: [
+            // PRIMERA OPCIÓN PARA PONER BORDER REDONDEADOS EN UNA IMAGEN
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                menuMap["urlImage"],
+                width: widthPageAux / 4,
+                height: widthPageAux / 4,
+                fit: BoxFit.cover,
               ),
-            ],
-          ),
-        ],
+            ),
+
+            // SEGUNDA FORMA PARA PONER BORDES REONDEADOS PARA UNA IMAGEN
+            // Container(
+            //   width: widthPage / 4,
+            //   height: widthPage / 4,
+            //   decoration: BoxDecoration(
+            //     borderRadius: BorderRadius.circular(12),
+            //     image: DecorationImage(
+            //       image: NetworkImage(
+            //         "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            //       ),
+            //       fit: BoxFit.cover,
+            //     ),
+            //   ),
+            // ),
+            SizedBox(width: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(menuMap["nombre"], style: TextStyle(fontSize: 22)),
+                Text(menuMap["dias"], style: TextStyle(fontSize: 18)),
+                Text(
+                  "S/ . ${menuMap["precio"]} ",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
