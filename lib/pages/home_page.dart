88000b9b1551c00a394a8764bd75f62dbfa321a5
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-05-30 19:47:54 -0500

    estructura del ccontiainer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double widthPage = MediaQuery.of(context).size.width;
+    double heightPage = MediaQuery.of(context).size.height;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Text(
+            "SetState Cards Assets App",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+          ),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Selecciona tu mejor opción",
+                style: TextStyle(fontSize: 24),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 16),
+                padding: EdgeInsets.all(16),
+                color: Colors.red,
+                child: Row(
+                  children: [
+                    Image.network(
+                      "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                      width: widthPage / 4,
+                      height: widthPage / 4,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Menú 1", style: TextStyle(fontSize: 22)),
+                        Text(
+                          "Lun - Mie - Vier",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                          "S/ . 5 ",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
